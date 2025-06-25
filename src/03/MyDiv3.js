97acf9f16f54daed0d2ff3104418ced9ee60d537
ru0dn7 <your.email@example.com>
2025-06-25 17:50:14 +0900

    probs 전달

--- src/03/MyDiv3.js
+++ src/03/MyDiv3.js
@@ -1,8 +1,10 @@
 
-export default function MyDiv3() {
+// export default function MyDiv3(probs) {
+export default function MyDiv3({d1, d2, d3}) {
   return (
     <div className="flex flex-col p-5 m-10 w-3/4 h-3/4 bg-lime-500 text-white">
-      MyDiv3
+      {/* {`${probs.d1} > ${probs.d2} > ${probs.d3}`} */}
+      {`${d1} > ${d2} > ${d3}`}
     </div>
-  )
+  );
 }
